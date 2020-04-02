9d1d53ab889e8d68fdde050b7dc535a6e9096f4c
Yuqing Du <yuqing_du@hotmail.com>
2020-04-02 07:01:58 -0700

    concat in policy

--- dreamer.py
+++ dreamer.py
@@ -159,7 +159,7 @@ class Dreamer(tools.Module):
     else:
       latent, action = state
     embed = self._encode(preprocess(obs, self._c))
-    state = tf.dtypes.cast(obs['state'], tf.float16)
+    state = tf.dtypes.cast(obs['state'], embed.dtype)
     embed = tf.concat([state, embed], axis=-1)
     latent, _ = self._dynamics.obs_step(latent, action, embed)
     feat = self._dynamics.get_feat(latent)
