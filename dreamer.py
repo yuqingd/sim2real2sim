7d67b90d42297d6376199f839d0f2932be0b2b7c
Yuqing Du <yuqing_du@hotmail.com>
2020-04-01 21:14:54 -0700

    remove debug

--- dreamer.py
+++ dreamer.py
@@ -180,8 +180,6 @@ class Dreamer(tools.Module):
   def _train(self, data, log_images):
     with tf.GradientTape() as model_tape:
       embed = self._encode(data)
-      import pdb;
-      pdb.set_trace()
       embed = tf.concat([data['state'], embed], axis=2)
       post, prior = self._dynamics.observe(embed, data['action'])
       feat = self._dynamics.get_feat(post)
