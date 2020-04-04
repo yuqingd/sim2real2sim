a10d8dc6a39e4064988359ce9c4bd949a276baa7
Yuqing Du <yuqing_du@hotmail.com>
2020-04-04 15:56:23 -0700

    Try default state param

--- dreamer.py
+++ dreamer.py
@@ -327,7 +327,7 @@ class Dreamer(tools.Module):
     truth = data['image'][:6] + 0.5
     recon = image_pred.mode()[:6]
     import pdb; pdb.set_trace()
-    init, _ = self._dynamics.observe(embed[:6, :5], data['action'][:6, :5], data['state'][:6, :5])
+    init, _ = self._dynamics.observe(embed[:6, :5], data['action'][:6, :5], data['state'][:6])
     init = {k: v[:, -1] for k, v in init.items()}
     prior = self._dynamics.imagine(data['action'][:6, 5:], init)
     openl = self._decode(self._dynamics.get_feat(prior)).mode()
