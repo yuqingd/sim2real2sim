46c5333ed413a97a0825ae2c3ed67873f5464755
AliengirlLiv <oliviagracewatkins@gmail.com>
2020-04-16 01:34:59 -0700

    add real world env

--- environments/push.py
+++ environments/push.py
@@ -11,7 +11,8 @@ MODEL_XML_PATH = os.path.join(FETCH_PATH, 'assets/fetch/push.xml')
 
 
 class FetchPushEnv(fetch_env.FetchEnv, utils.EzPickle):
-    def __init__(self, use_vision=True, reward_type='dense', deterministic=True, distance_threshold=0.1):
+    def __init__(self, use_vision=True, reward_type='dense', deterministic=True, distance_threshold=0.1,
+                 real_world=False):
         initial_qpos = {
             'robot0:slide0': 0.405,
             'robot0:slide1': 0.48,
@@ -22,5 +23,6 @@ class FetchPushEnv(fetch_env.FetchEnv, utils.EzPickle):
             self, MODEL_XML_PATH, has_object=True, block_gripper=True, n_substeps=20,
             gripper_extra_height=0.0, target_in_the_air=False, target_offset=0.0,
             obj_range=0.15, target_range=0.15, distance_threshold=distance_threshold,
-            initial_qpos=initial_qpos, reward_type=reward_type, use_vision=use_vision, deterministic=deterministic)
+            initial_qpos=initial_qpos, reward_type=reward_type, use_vision=use_vision, deterministic=deterministic,
+            real_world=real_world)
         utils.EzPickle.__init__(self, use_vision, reward_type, deterministic, distance_threshold)
