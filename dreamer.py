e53696c3dfb45044bb996a62cf897d49e85e84db
Yuqing Du <yuqing_du@hotmail.com>
2020-05-11 09:24:36 -0700

    increase alpha to 0.5

--- dreamer.py
+++ dreamer.py
@@ -583,7 +583,7 @@ def main(config):
             prev_mean, prev_range = env.dr["body_mass"]
             pred_mean = real_pred_sim_params[0]
             pred_range = real_pred_sim_params[1]
-            alpha = 0.1
+            alpha = 0.5
 
             new_mean = prev_mean*(1-alpha) + alpha*pred_mean
             new_range = prev_range*(1-alpha) + alpha*pred_range
