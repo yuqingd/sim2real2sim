4e6c9181094ff0670a9fa193096bae7eeed0f6a7
AliengirlLiv <oliviagracewatkins@gmail.com>
2020-06-14 13:29:19 -0700

    add tests

--- test_end_effector_control.py
+++ test_end_effector_control.py
@@ -1,3 +1,41 @@
+import numpy as np
+import wrappers
+import moviepy.editor as mpy
+from matplotlib import pyplot as plt
+env = wrappers.Kitchen()
+
+# ====================== check whether end_effector control works ================
+
+
+# Axis aligned
+end_effector_index = env.end_effector_index
+for i in range(3):
+    o = env.reset()
+    frames = [o['image']]
+    positions = [env._env.sim.data.site_xpos[end_effector_index].copy()]
+    a = np.zeros((3,))
+    a[i] = 1  # Specify a change along one axis. We could also comment this out to check that with no change the arm stays still.
+
+    for _ in range(100):
+        o, _, _, _ = env.step(a)
+        frames.append(o['image'])
+        positions.append(env._env.sim.data.site_xpos[end_effector_index].copy())
+    fps = 10
+    clip = mpy.ImageSequenceClip(frames, fps=fps)
+    clip.write_gif('test_end_effector_axis' + str(i) + '.gif', fps=fps)
+
+    # We'd expect to see a graph where all positions stay the same except the one along which we're moving.
+    for j in range(3):
+        data = [p[j] for p in positions]
+        plt.plot(range(len(data)), data)
+    plt.legend(['x', 'y', 'z'])
+    plt.savefig('plot_end_effector_axis' + str(i) + '.png')
+    plt.show()
+
+
+# ==============================================================================================
+
+
 import wrappers
 import numpy as np
 from dm_control.utils.inverse_kinematics import qpos_from_site_pose
