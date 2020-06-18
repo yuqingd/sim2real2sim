cefb8bf7fa0ef249b91d05d835ad414dd944d4ac
Yuqing Du <yuqing_du@hotmail.com>
2020-06-17 17:43:43 -0700

    Update renderer

--- wrappers.py
+++ wrappers.py
@@ -280,9 +280,9 @@ class Kitchen:
   def render(self, *args, **kwargs):
     if kwargs.get('mode', 'rgb_array') != 'rgb_array':
       raise ValueError("Only render mode 'rgb_array' is supported.")
-    camera = engine.MovableCamera(self._env.sim, 1920, 2560)
+    camera = engine.MovableCamera(self._env.sim, *self._size)
     camera.set_pose(distance=2.2, lookat=[-0.2, .5, 2.], azimuth=70, elevation=-35)
-    img = camera.render(self._size)
+    img = camera.render()
 
     #img = self._env.render(mode='rgb_array')
     #return img # TODO: later rethink whether we want the image cropped and resized or not
