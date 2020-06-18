0f7c5e7142f86012b12e5e9d52a49dae4e573594
AliengirlLiv <oliviagracewatkins@gmail.com>
2020-06-18 07:48:15 -0700

    add sim params size

--- dreamer.py
+++ dreamer.py
@@ -148,6 +148,7 @@ def define_config():
 
   config.outer_loop_version = 0  # 0= no outer loop, 1 = regression, 2 = conditioning
   config.alpha = 0.5
+  config.sim_params_size = 0
 
   return config
 
