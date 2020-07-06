40da75cb80e1dab520755d9335745ffb9ce59072
AliengirlLiv <oliviagracewatkins@gmail.com>
2020-07-05 17:54:18 -0700

    update hparams for runs

--- generate_run_commands.py
+++ generate_run_commands.py
@@ -6,18 +6,25 @@ PRESENT = 'present'
 ABSENT = 'absent'
 
 # This is to grid search. If you don't want to grid search, manually write in the param_args list.
+
 sweep_params = {
     "task": ["kitchen_push_kettle_burner", "kitchen_slide_kettle", "kitchen_pick_kettle"],
-    "outer_loop_version": [0],
+    "outer_loop_version": [0, 1, 2],
     "sample_real_every": [100],
-    "seed": [0],
+    "seed": [0, 1],
     "steps": [2000000],
-    "time_limit": [100],
-    "step_repeat": [10, 100, 200],
-    "step_size": [.05, .2],
-    "batch_length": [10, 25],
+    "time_limit": [200],
+    "step_repeat": [10],
+    "step_size": [.05],
+    "batch_length": [10],
+    "simple_randomization": [True],
+    "mass_mean": [.02],
+    "mass_range": [.1],
+    "dr": [PRESENT],
 }
-starting_id = 0
+
+
+starting_id = 36
 
 print(f"{1:02d}")
 
@@ -63,3 +70,66 @@ print("\n".join(arg_mapping_strs))
 print(args_dict)
 with open('args_dict.pkl', 'wb') as f:
     pickle.dump(args_dict, f)
+
+
+# PAST RUN COMMANDS
+# 0-35
+# sweep_params = {
+#     "task": ["kitchen_push_kettle_burner", "kitchen_slide_kettle", "kitchen_pick_kettle"],
+#     "outer_loop_version": [0],
+#     "sample_real_every": [100],
+#     "seed": [0],
+#     "steps": [2000000],
+#     "time_limit": [100],
+#     "step_repeat": [10, 100, 200],
+#     "step_size": [.05, .2],
+#     "batch_length": [10, 25],
+# }
+
+# 36-53
+# sweep_params = {
+#     "task": ["kitchen_push_kettle_burner", "kitchen_slide_kettle", "kitchen_pick_kettle"],
+#     "outer_loop_version": [0, 1, 2],
+#     "sample_real_every": [100],
+#     "seed": [0, 1],
+#     "steps": [2000000],
+#     "time_limit": [200],
+#     "step_repeat": [10],
+#     "step_size": [.05],
+#     "batch_length": [10],
+#     "simple_randomization": [True],
+#     "mass_mean": [.02],
+#     "mass_range": [.1],
+#     "dr": [PRESENT],
+# }
+
+# 54-71
+# sweep_params = {
+#     "task": ["kitchen_push_kettle_burner", "kitchen_slide_kettle", "kitchen_pick_kettle"],
+#     "outer_loop_version": [0, 1, 2],
+#     "sample_real_every": [100],
+#     "seed": [0, 1],
+#     "steps": [2000000],
+#     "time_limit": [200],
+#     "step_repeat": [10],
+#     "step_size": [.05],
+#     "batch_length": [10],
+#     "dr_option": ['inaccurate_large_range'],
+#     "mass_mean": [.02],
+#     "mass_range": [.1],
+#     "dr": [PRESENT],
+# }
+
+# 72-77
+# sweep_params = {
+#     "task": ["kitchen_push_kettle_burner", "kitchen_slide_kettle_burner", "kitchen_pick_kettle_burner"],
+#     "outer_loop_version": [0],
+#     "sample_real_every": [100],
+#     "seed": [0, 1],
+#     "steps": [2000000],
+#     "time_limit": [200],
+#     "step_repeat": [10],
+#     "step_size": [.05],
+#     "batch_length": [10],
+#     "dr": [ABSENT],
+# }
