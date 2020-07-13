2394da156f2090ceeec4af95cf5f30959e8a603d
Yuqing Du <yuqing_du@hotmail.com>
2020-07-12 19:50:01 -0700

    Increase cylinder mass

--- wrappers.py
+++ wrappers.py
@@ -299,7 +299,7 @@ class Kitchen:
 
 
       d2 = np.linalg.norm(kettle - self.goal)
-      done = np.abs(d2) < 0.3
+      done = np.abs(d2) < 0.1
 
       if self.slide_d1 is not None:
         reward = -(self.slide_d1 + d2)
