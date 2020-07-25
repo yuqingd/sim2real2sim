b0348a655967e92d53c1de5d95c67d39ba70ae51
Yuqing Du <yuqing_du@hotmail.com>
2020-07-25 11:58:36 -0700

    fix import

--- wrappers.py
+++ wrappers.py
@@ -966,7 +966,7 @@ class Kitchen:
 class MetaWorld:
   def __init__(self, name, size=(64, 64), mean_only=False, early_termination=False, dr_list=[], simple_randomization=False, dr_shape=None, outer_loop_version=0,
                real_world=False, dr=None, use_state=False, azimuth=-30, distance=2, elevation=-20, use_depth=False):
-    from metaworld import ML1
+    from environments.metaworld.metaworld import ML1
     import random
     self._ml1 = ML1(name + "-v1")
     self._env = self._ml1.train_classes[name + "-v1"]()
