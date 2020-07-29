88cfbceff990b954f459736d697599d34eb18926
AliengirlLiv <oliviagracewatkins@gmail.com>
2020-07-28 20:01:49 -0700

    update run commands

--- generate_run_commands.py
+++ generate_run_commands.py
@@ -7,169 +7,43 @@ PRESENT = 'present'
 ABSENT = 'absent'
 
 # This is to grid search. If you don't want to grid search, manually write in the param_args list.
-starting_id = 884
+starting_id = 2292
 
 sweep_params_1 = [
-    [{"task": ["kitchen_push_kettle_burner", "kitchen_slide_kettle_burner", "kitchen_push_kettle", ],
-      "alpha": [0.1],
-      "dr_option": ["partial_dr"],
-      "seed": [2, 3, 4,],
-      },
-     {"task": ["kitchen_slide_kettle",],
-      "alpha": [0.1],
-      "dr_option": ["partial_dr"],
-      "seed": [2, 3, 4, 5, 6],
-      },
-     {"task": ["kitchen_open_cabinet"],
-      "alpha": [0.3],
-      "dr_option": ["partial_dr"],
-      "seed": [2, 3, 4, 5, 6],
-      },
-     {"task": ["kitchen_rope",],
-      "alpha": [0.3],
-      "dr_option": ["all_dr"],
-      "seed": [2, 3, 4,],
-      },
-     {"task": ["dmc_cup_catch", "dmc_walker_walk", "dmc_finger_spin", "dmc_cheetah_run"],
-      "alpha": [0.3],
-      "dr_option": ["all_dr"],
-      "time_limit": [1000],
-      "random_crop": [True],
-      "seed": [2, 3, 4,],
-      }],
-    # Oracle and baseline (suite 1, 2).  Note that alpha will not be used here and dr_option won't be used in the baseline.
-    [{"dr": [PRESENT, ABSENT],
-      "sample_real_every": [3e6],
-      "real_world_prob": [0],
-      "outer_loop_version": [0],},
-     # outer loop 1, no inner loop (suite 5)
-     {"dr": [PRESENT],
-      "sample_real_every": [3e6],
-      "real_world_prob": [0],
-      "outer_loop_version": [1],
+   [{"task": ["kitchen_open_cabinet",
+              "kitchen_push_kettle_burner",
+              "kitchen_rope",
+              ],
+     "horizon": [8, 25],
+     "time_limit": [250, 500],
      },
-     # outer loop conditions (suite 3,4,6)
-    {"dr": [PRESENT],
-      "sample_real_every": [100],
-      "outer_loop_version": [0, 1, 2],
-     }],
-    [
-     {"mean_scale": [5], "range_scale": [.5]},
-     {"mean_scale": [1], "range_scale": [5]},
-      ],
-    [{"log_images": [False]}],
-    [{"buffer_size": [2000]}],
-    [{"early_termination": [False]}],
-]
 
+    {"task": ["metaworld_faucet-open",
+              "metaworld_stick-push",
+              "metaworld_stick-pull",
+              "metaworld_hammer",
+              "metaworld_plate-slide-back",
+              "metaworld_drawer-open",
+              "metaworld_box-close",
 
-# ABLATIONS
-sweep_params_2 = [
-    # Suites
-    [{"task": ["kitchen_push_kettle_burner",],
-      "alpha": [0.1],
-      "dr_option": ["partial_dr"],
-      "mean_scale": [5], "range_scale": [.5],
-      "outer_loop_version": [2],
-      },
-     {"task": ["kitchen_push_kettle",],
-      "alpha": [0.1],
-      "dr_option": ["partial_dr"],
-      "mean_scale": [5], "range_scale": [.5],
-      "outer_loop_version": [1],
-      },
-     {"task": ["kitchen_push_kettle",],
-      "alpha": [0.1],
-      "dr_option": ["partial_dr"],
-      "mean_scale": [1], "range_scale": [5],
-      "outer_loop_version": [2],
-      },
-     {"task": ["kitchen_rope",],
-      "alpha": [0.3],
-      "dr_option": ["all_dr"],
-      "mean_scale": [.1,], "range_scale": [.01],
-      "outer_loop_version": [1],
-      },
-     ],
+              ]}
 
-
-    # Ablations
-    [# Different amounts of sample_real_every
-      {"sample_real_every": [10, 1000],
-      },
-     # Different amounts of eval_every
-      {"eval_every": [1000, 100000],
-       "sample_real_every": [100],
-      },
-    # Different amounts of interventions
-    {"sample_real_every": [200],
-     "num_real_world": [2],
-    },
-    {"sample_real_every": [1000],
-     "num_real_world": [10],
-    },
     ],
-
-    # Constant parameters
-    [{"dr": [PRESENT]}],
-    [{"log_images": [False]}],
-    [{"buffer_size": [2000]}],
-    [{"early_termination": [False]}],
     [{"seed": [0, 1, 2]}],
-]
-
-# Different amounts of alpha
-sweep_params_3 = [
-    # Suites
-[{"task": ["kitchen_push_kettle_burner",],
-      "alpha": [0.3, 0.6, 0.9],
-      "dr_option": ["partial_dr"],
-      "mean_scale": [5], "range_scale": [.5],
-      "outer_loop_version": [2],
-      },
-     {"task": ["kitchen_push_kettle",],
-      "alpha": [0.3, 0.6, 0.9],
-      "dr_option": ["partial_dr"],
-      "mean_scale": [5], "range_scale": [.5],
-      "outer_loop_version": [1],
-      },
-     {"task": ["kitchen_push_kettle",],
-      "alpha": [0.3, 0.6, 0.9],
-      "dr_option": ["partial_dr"],
-      "mean_scale": [1], "range_scale": [5],
-      "outer_loop_version": [2],
-      },
-     {"task": ["kitchen_rope",],
-      "alpha": [0.1, 0.6, 0.9],
-      "dr_option": ["all_dr"],
-      "mean_scale": [.1,], "range_scale": [.01],
-      "outer_loop_version": [1],
-      },
-     ],
-
-    # Constant parameters
-    [{"sample_real_every": [100]}],
-    [{"dr": [PRESENT]}],
+    [{"real_world_prob": [0]}],
+    [{"sample_real_every": [3e6]}],
     [{"log_images": [False]}],
-    [{"buffer_size": [2000]}],
     [{"early_termination": [False]}],
-    [{"seed": [0, 1,2]}],
 ]
-
-
-
-
-
-# 1176 (default)
-# Only 3 seeds for ablations
 
 sweep_params_list = [
     sweep_params_1,
-    sweep_params_2, sweep_params_3
 ]
 
 id = starting_id
 total_num_commands = 0
+args_dict = {}
+
 
 for sweep_params in sweep_params_list:
     # Each param set has a group of parameters.  We will find each product of groups of parameters
@@ -198,7 +72,7 @@ for sweep_params in sweep_params_list:
 
     command_strs = []
     arg_mapping_strs = []
-    args_dict = {}
+
 
 
     for args in param_args:
@@ -231,6 +105,6 @@ print("TOTAL COMMANDS", total_num_commands)
 # We save this object in case it's easier to load it into Jupyter notebook later and extract info from it for plotting
 # rather than doing it manually.
 # print(args_dict)
-# with open('args_dict.pkl', 'wb') as f:
-#     pickle.dump(args_dict, f)
+with open(f'args_dict{starting_id}.pkl', 'wb') as f:
+    pickle.dump(args_dict, f)
 
