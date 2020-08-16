99fe316b19881957811b38c0006bbbc93273b3e0
AliengirlLiv <oliviagracewatkins@gmail.com>
2020-08-15 22:22:04 -0700

    add new dr option

--- dreamer.py
+++ dreamer.py
@@ -365,16 +365,13 @@ def config_dr(config):
 
 
       config.sim_params_size = len(config.real_dr_list)
-      if dr_option in ['all_dr', 'partial_dr', 'mass', 'visual', 'friction']:
-        mean_scale = config.mean_scale
-        range_scale = config.range_scale
-        config.dr = {}  # (mean, range)
-        for key, real_val in config.real_dr_params.items():
-          if real_val == 0:
-            real_val = 5e-2
-          config.dr[key] = (real_val * mean_scale, real_val * range_scale)
-      else:
-        raise NotImplementedError(dr_option)
+      mean_scale = config.mean_scale
+      range_scale = config.range_scale
+      config.dr = {}  # (mean, range)
+      for key, real_val in config.real_dr_params.items():
+        if real_val == 0:
+          real_val = 5e-2
+        config.dr[key] = (real_val * mean_scale, real_val * range_scale)
 
         #Keep mean only
     if config.mean_only and config.dr is not None:
